a448348681cd6bbd1c28ced32d740cb7636a4dc2
janiceyoshimura <janiceyoshimura@gmail.com>
2020-03-16 21:27:14 +0000

    First Commit

--- app.rb
+++ app.rb
@@ -14,5 +14,116 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-events_table = DB.from(:events)
-rsvps_table = DB.from(:rsvps)
+rotations_table = DB.from(:rotations)
+interests_table = DB.from(:interests)
+comments_table = DB.from(:comments)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
+
+# homepage and list of rotations (aka "index")
+get "/" do
+    puts "params: #{params}"
+
+    @rotations = rotations_table.all.to_a
+    pp @rotations
+
+    view "rotations"
+end
+
+# rotations details (aka "show")
+get "/rotations/:id" do
+    puts "params: #{params}"
+
+    @users_table = users_table
+    @rotation = rotations_table.where(id: params[:id]).to_a[0]
+    pp @rotation
+
+    @comment = comments_table.where(event_id: @event[:id]).to_a
+
+    view "event"
+end
+
+# display the comment form (aka "new")
+get "/rotations/:id/comment/new" do
+    puts "params: #{params}"
+
+    @rotation = rotations_table.where(id: params[:id]).to_a[0]
+    view "new_comment"
+end
+
+# receive the submitted comment form (aka "create")
+post "/rotations/:id/comment/create" do
+    puts "params: #{params}"
+
+    # first find the event that rsvp'ing for
+    @rotation = rotations_table.where(id: params[:id]).to_a[0]
+    # next we want to insert a row in the rsvps table with the rsvp form data
+    comments_table.insert(
+        rotation_id: @rotation[:id],
+        user_id: session["user_id"],
+        comments: params["comment_detail"],
+    )
+
+    redirect "/rotations/#{@rotation[:id]}"
+end
+
+# display the signup form (aka "new")
+get "/users/new" do
+    view "new_user"
+end
+
+# receive the submitted signup form (aka "create")
+post "/users/create" do
+    puts "params: #{params}"
+
+    # if there's already a user with this email, skip
+    existing_user = users_table.where(email: params["email"]).to_a[0]
+    if existing_user
+        view "error"
+    else
+        users_table.insert(
+            first_name: params["first_name"],
+            last_name: params["last_name"],
+            email: params["email"],
+            password: BCrypt::Password.create(params["password"])
+        )
+
+        redirect "/logins/new"
+    end
+end
+
+# display the login form (aka "new")
+get "/logins/new" do
+    view "new_login"
+end
+
+# receive the submitted login form (aka "create")
+post "/logins/create" do
+    puts "params: #{params}"
+
+    # step 1: user with the params["email"] ?
+    @user = users_table.where(email: params["email"]).to_a[0]
+
+    if @user
+        # step 2: if @user, does the encrypted password match?
+        if BCrypt::Password.new(@user[:password]) == params["password"]
+            # set encrypted cookie for logged in user
+            session["user_id"] = @user[:id]
+            redirect "/"
+        else
+            view "create_failed_login"
+        end
+    else
+        view "create_failed_login"
+    end
+end
+
+# logout user
+get "/logout" do
+    # remove encrypted cookie for logged out user
+    session["user_id"] = nil
+    redirect "/logins/new"
+end
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,65 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :rotations do
   primary_key :id
-  String :title
-  String :description, text: true
-  String :date
-  String :location
+  String :company
+  String :address
+  String :city
+  String :function
+  String :function_description
+  String :length
 end
-DB.create_table! :rsvps do
+DB.create_table! :comments do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
-  String :name
+  foreign_key :rotation_id
+  String :first_name
   String :email
-  String :comments, text: true
+  String :comment_detail, text: true
+end
+
+DB.create_table! :interests do
+  primary_key :id
+  foreign_key :user_id
+  Boolean :interested
+end
+
+DB.create_table! :users do
+  primary_key :id
+  String :first_name
+  String :last_name
+  String :email
+  String :password
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+rotations_table = DB.from(:rotations)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+rotations_table.insert(company: "Allbirds", 
+                    address: "730 Montgomery St, San Francisco, CA 94111",
+                    city: "San Francisco, CA",
+                    function: "Operations",
+                    function_description: "Kellogg Global Hub",
+                    length: "6 months")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+rotations_table.insert(company: "McDonalds", 
+                    address: "1035 W Randolph St, Chicago, IL 60607",
+                    city: "Chicago, IL",
+                    function: "Global Strategy",
+                    function_description: "Kellogg Global Hub",
+                    length: "12 months")
+
+rotations_table.insert(company: "Estee Lauder", 
+                    address: "767 Fifth Avenue New York, NY 10153",
+                    city: "New York, NY",
+                    function: "Merchandising",
+                    function_description: "Kellogg Global Hub",
+                    length: "8 months")
+
+rotations_table.insert(company: "Amazon", 
+                    address: "410 Terry Ave N, Seattle, WA 98109",
+                    city: "Seattle, WA",
+                    function: "Logistics",
+                    function_description: "Kellogg Global Hub",
+                    length: "6 months")
+
